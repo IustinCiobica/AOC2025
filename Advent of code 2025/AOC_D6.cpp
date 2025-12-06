71431beee199d899ae447de41e7b2abd19b711ff
Iustin <ciobicaiustin2@gmail.com>
2025-12-06 17:31:49 +0200

    Day 6

--- Advent of code 2025/AOC_D6.cpp
+++ Advent of code 2025/AOC_D6.cpp
@@ -63,7 +63,104 @@ void part1()
 	cout << "answer: " << answer;
 }
 
+int isAllSpace(char** inputText, int col, int noOfRows)
+{
+	int ok = 1;
+	for (int i = 0; i < noOfRows; i++)
+	{
+		if (inputText[i][col] != ' ')
+			return 0;
+	}
+	return ok;
+}
+
+void part2()
+{
+	// I store the input 
+	ifstream input("input.txt");
+
+	char** inputText = new char* [10];
+	for (int i = 0; i < 10; i++)
+		inputText[i] = new char[10000];
+
+	char line[10000];
+	int inputRowSize = 0;
+	while (input.getline(line, sizeof(line)))
+	{
+		strcpy(inputText[inputRowSize], line);
+		inputRowSize++;
+	}
+
+	for (int i = 0; i < inputRowSize; i++)
+		cout << inputText[i] << endl;
+
+	// I put operators in an array
+	int noOps = 0;
+	int operatorArr[1000];
+	for (int i = 0; i < strlen(inputText[inputRowSize-1]); i++)
+	{
+		if (inputText[inputRowSize - 1][i] != ' ')
+		{
+			operatorArr[noOps] = inputText[inputRowSize - 1][i];
+			noOps++;
+		}
+	}
+	
+	for (int i = 0; i < noOps; i++)
+		cout << operatorArr[i] << " ";
+
+	// Apply the operations
+	unsigned long long int answer = 0;
+	int currentOp = 0;
+	unsigned long long tempAnswer = 0;
+	long long int number;
+
+	if (operatorArr[currentOp] == 42)
+		tempAnswer = 1;
+	else
+		tempAnswer = 0;
+
+	for (int j = 0; j < strlen(inputText[0]); j++)
+	{
+		int ok = 0;
+		number = 0;
+		if (isAllSpace(inputText, j, inputRowSize - 1) == 0)
+		{
+			ok = 1; // it is okay to use the number
+		}
+		else
+		{
+			currentOp++;
+			answer += tempAnswer;
+			if (operatorArr[currentOp] == 42)
+				tempAnswer = 1;
+			else
+				tempAnswer = 0;
+		}
+
+		for (int i = 0; i < inputRowSize - 1; i++)
+		{
+			if (inputText[i][j] != ' ')
+			{
+				number = number*10 + (inputText[i][j] - '0');
+			}
+		}
+		
+		if (ok)
+		{
+			if (operatorArr[currentOp] == 42)
+				tempAnswer *= number;
+			else
+				tempAnswer += number;
+		}
+	}
+
+	answer += tempAnswer;
+
+	cout <<endl << "answer: " << answer;
+}
+
 int main()
 {
-	part1();
+	part2();
 }
